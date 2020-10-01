1ece61d8bb0bdfde98620f4ab2737228bd99d950
Oksana Bolbat <oksana.bolbat@gmail.com>
2020-10-01 19:37:20 +0300

    init: functions created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) === true) {
+        return Math.min(array);
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) === true) {
+        return Math.min(array);
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) === true) {
+        let sum = 0;
+        array.array.forEach((element) => {
+            if (!isNaN(element)) {
+                sum += element;
+            } else {
+                break;
+            }
+        });
+    }
+    if (sum != 0) {
+        return sum / array.length;
+    }
+    return 0;
+};
